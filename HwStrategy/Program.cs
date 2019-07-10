f8933516e507c5e03df9cf684db0090f9e293b79
Islam <denizzz9998882@gmail.com>
2019-07-10 23:44:11 +0600

    first commit

---
+++ HwStrategy/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Transport auto = new Transport(new EarthMove());
+        auto.Move();
+        auto.Movable = new WaterMove();
+        auto.Move();
+
+        Console.ReadLine();
+    }
+}
+interface IMovable
+{
+    void Move();
+}
+
+class EarthMove : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Перемещение по земле");
+    }
+}
+
+class WaterMove : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Перемещение по воде");
+    }
+}
+class Transport
+{
+
+    public Transport(IMovable mov)
+    {
+        Movable = mov;
+    }
+    public IMovable Movable { private get; set; }
+    public void Move()
+    {
+        Movable.Move();
+    }
+}
